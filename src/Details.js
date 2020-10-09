7bfd9bcf0da6ff1414f86e027a8ab1a549c0c16a
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2020-10-09 10:02:04 +0530

    Adding Error boundaries and 404 Redirect page

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from './ErrorBoundary';
 
 // Use of Class Components
 class Details extends React.Component {
@@ -15,6 +16,8 @@ class Details extends React.Component {
   state = { loading: true };
 
   componentDidMount() {
+    // throw new Error('sorry');
+    // Above line to throw error (testing purposes)
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -49,4 +52,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  )
+}
